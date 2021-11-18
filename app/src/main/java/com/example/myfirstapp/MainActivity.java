087c06840b803dfe4f41818a25a32f7a57e1031c
C M <cm@cmedina2.student.iastate.edu>
2021-11-18 10:37:44 -0600

    added google-services and dependencies, updated the sdk

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
